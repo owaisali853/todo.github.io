5bb2edc2b61c842259025ee5659cd6f3f13bb4f4
unknown <oa075855@gmail.com>
2022-11-13 22:26:56 +0500

    comit

---
+++ app.js
@@ -0,0 +1,55 @@
+function editItem(e){
+    var val = e.parentNode.firstChild.nodeValue;
+   var editValue = prompt("ENTER EDIT VALUE")
+   
+   e.parentNode.firstChild.nodeValue = editValue;
+}
+
+
+var list = document.getElementById('list');
+
+function addTodo(){
+    var todo = document.getElementById('todo-item')
+    var li = document.createElement('li')
+    var liText = document.createTextNode(todo.value)
+    li.appendChild(liText)
+    list.appendChild(li)
+    todo.value =" "
+
+    var delBtn = document.createElement("button")
+    var delText = document.createTextNode("DELETE")
+    delBtn.setAttribute("class","btn")
+    delBtn.setAttribute("onclick", "deleteItem(this)")
+    delBtn.appendChild(delText)
+
+    var editBtn = document.createElement("button")
+    var editText = document.createTextNode("EDIT")
+    editBtn.appendChild(editText)
+    editBtn.setAttribute("onclick","editItem(this)")
+
+    li.appendChild(delBtn)
+    li.appendChild(editBtn)
+   
+}
+
+function deleteItem(e){
+    e.parentNode.remove()
+}
+
+
+
+function deleteAll(){
+    list.innerHTML = " "
+}
+
+// function saveData(){
+//     var button = document.getElementById("button")
+//     var btn = document.getElementById("btn")
+    
+//     var data = {
+//         button: button.value,
+//         btn: btn.value,
+//     }
+//     console.log(data)
+// }
+console.log(firebase)
